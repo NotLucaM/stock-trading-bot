3e410aef842c04d4e448299127ab3e6cdd6d536f
Luca Manolache <luca1230@gmail.com>
2020-12-19 12:30:01 -0800

    added buy and sell operations

--- portfolio_manger.py
+++ portfolio_manger.py
@@ -5,7 +5,17 @@ from datetime import datetime
 import alpaca_trade_api as tradeapi
 import yfinance as yf
 
-global api = tradeapi.REST()
+import KEYS
+
+global api
+api = tradeapi.REST(KEYS.TRADE_KEY_ID, KEYS.TRADE_API_SECRET_KEY, base_url="https://paper-api.alpaca.markets")
+global net_money
+
+purchased = []
 
 def buy(ticker: str, quantity: int):
-    tradeapi
+    api.submit_order(symbol=ticker, qty=quantity, side='buy', type='market', time_in_force='day')
+
+
+def sell(ticker: str, quantity: int):
+    api.submit_order(symbol=ticker, qty=quantity, side='sell', type='market', time_in_force='day')
