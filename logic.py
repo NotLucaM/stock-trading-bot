b8d5e23478f87c914e3163fc457e0baf6f6bec7b
Luca Manolache <luca1230@gmail.com>
2020-12-22 19:34:40 -0800

    Added logic class in order to deal with logic of the program.

---
+++ logic.py
@@ -0,0 +1,31 @@
+import numpy as np
+import portfolio_manger
+
+
+global lookback
+lookback = {}
+
+
+ceiling, floor = 30, 10
+
+def init():
+    for ticker in portfolio_manger.available:
+        lookback[ticker] = 30
+
+
+def on_market_open(ticker: str):
+    # determine the look back length based on volatility
+    close = portfolio_manger.get_data(ticker, period='1m')['Close']
+    today_volatility = np.std(close[1:31])
+    yesterday_volatility = np.std(close[0:30])
+    delta_volatility = (today_volatility - yesterday_volatility) / today_volatility
+    lookback[ticker] = round(lookback[ticker] * (1 + delta_volatility))
+
+    if lookback[ticker] > ceiling:
+        lookback[ticker] = ceiling
+    elif lookback[ticker] < floor:
+        lookback[ticker] = floor
+
+def on_update(ticker: str):
+    high = portfolio_manger.get_data(ticker)
+
